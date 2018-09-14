2139c962188679f8ccbaac24fc5c2d06a8fc0d40
ontofractal <ontofractal@protonmail.com>
2018-09-14 13:08:17 +0100

    init new project ( °-°)シ ミ★ ミ☆

---
+++ lib/ex_coinmetrics.ex
@@ -0,0 +1,18 @@
+defmodule ExCoinmetrics do
+  @moduledoc """
+  Documentation for ExCoinmetrics.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCoinmetrics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_coinmetrics_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCoinmetricsTest do
+  use ExUnit.Case
+  doctest ExCoinmetrics
+
+  test "greets the world" do
+    assert ExCoinmetrics.hello() == :world
+  end
+end
